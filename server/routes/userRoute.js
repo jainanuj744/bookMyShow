2e90d356efa4a3a7affe7957103b3e9c480ef77b
Anuj Jain <jainanuj744@gmail.com>
2024-01-27 12:22:32 +0530

    login route setup

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,7 @@
 
 const router = require('express').Router();
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 router.post('/register',async (req,res)=>{
 
@@ -14,6 +15,12 @@ router.post('/register',async (req,res)=>{
                 messege:"User Already Exists"
             });    
         }
+
+        //hashed the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password,salt);
+        req.body.password = hashedPassword;
+
         const newUser = await User(req.body)
         console.log(newUser);
         await newUser.save();
@@ -24,6 +31,33 @@ router.post('/register',async (req,res)=>{
     }catch(err){
         console.log(err);
     }
+})
+
+router.post('/login', async (req,res)=>{
+    const user = await User.findOne({email:req.body.email})
+    if(!user){
+        return res.send({
+            success:false,
+            messege:"User does not exist"
+        })
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    if(!validPassword){
+        return res.send({
+            success:false,
+            messege:"Invalid Password"
+        })
+    }
+
+    return res.send({
+        success:true,
+        messege:"User Logged In"
     })
+
+
+
+})
     
 module.exports = router;
