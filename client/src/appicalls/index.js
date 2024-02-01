ea76a20663509438cc6534f75cb9e7abb456a9aa
Anuj Jain <jainanuj744@gmail.com>
2024-02-01 23:53:21 +0530

    jwt part 2 integrated

--- client/src/appicalls/index.js
+++ client/src/appicalls/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    headers: { 'Content-Type':'application/json'}
+    headers: { 
+        'Content-Type':'application/json',
+        'authorization':`Bearer ${localStorage.getItem('token')}`
+    }
+
 })
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/register',async (req,res)=>{
 
@@ -53,11 +54,20 @@ router.post('/login', async (req,res)=>{
     }
 
     const token = jwt.sign({userId:user._id},process.env.secret_key_jwt,{expiresIn:'1d'});
-    // console.log(token);
+    console.log(token);
     res.send({
         success:true,
         messege:"User Logged In",
         token:token
+    })
+})
+
+router.get("/get-current-user", authMiddleware, async (req,res)=>{
+    const user = await User.findById(req.body.userId).select('-password')
+    res.send({
+        success:true,
+        meassege:"You are allowed to go to protected route",
+        data:user
     })
 })
     
